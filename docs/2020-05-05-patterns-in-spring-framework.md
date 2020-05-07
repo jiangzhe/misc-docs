2f5da6d0b09a2f9e9bfa8fa30bd04216225367c2
Zhe Jiang <nju.jiangzhe@gmail.com>
2020-05-07 08:45:30 +0800

    update sequence

---
+++ docs/2020-05-05-patterns-in-spring-framework.md
@@ -0,0 +1,64 @@
+# Patterns in Spring Framework
+
+## 一个Spring Boot Web Server的启动
+
+- application.yml / application.properties 是在何时通过什么方式加载的？
+- 补充：为什么要用这种方式？
+
+
+
+- @Value, @PropertySource, @ConfigurationProperties
+
+
+## SpringFactoriesLoader
+
+## Annotation一览
+
+- @Controller, @Service, @Component, @Repository
+
+- @RequestMapping
+
+- @Scheduled, @Cacheable
+
+- @Transactional
+
+## MVC
+
+
+## Security Pattern
+
+
+## Starter Pattern
+
+## Lifecycle of application and beans
+
+- InitializingBean, DisposableBean
+
+- ApplicationListener
+
+- @PostConstruct
+
+- SmartInitializingSingleton
+
+- ApplicationRunListener
+
+
+
+1. Spring使用了几种日志库，是如何处理他们之间的关系的，在启动时如何保证日志配置的正确加载？
+
+2. Spring使用的是哪个ClassLoader，是在什么时候切换的？为什么要使用一个特定的ClassLoader？
+
+3. 依赖注入有几种方式？分别是如何实现的？适用/不适用于哪些场景？
+
+4. HttpServletRequest和HttpServletResponse是通过什么方式注入到Controller的方法中的？
+
+5. Spring为什么要实现自己的类读取器MetadataReader，直接使用Java的反射存在什么问题？
+
+ConfigurationClassParser.retrieveBeanMethodMetadata()
+
+6. AutoConfiguration究竟是通过什么机制定义Bean的？@ConditionalOnMissingBean实在什么时候判定的？
+
+7. 如何写一个spring boot starter
+
+@Enable*, @Import, ImportAware, spring.factories
+
---
+++ docs/draft/spring-boot-sequence.groovy
@@ -0,0 +1,133 @@
+App.main(args) {
+  SpringApplication.run(App.class, args) {
+    SpringApplicationRunListener.starting()
+    prepareEnvironment() {
+      StandardServletEnvironment.customizePropertySources() {
+        addLast(systemPropertiesPropertySource)
+        addLast(systemEnvironmentPropertySource)
+      }
+      configureProfiles()
+      SpringApplicationRunListener.environmentPrepared() {
+        // ApplicationEventMulticaster.multicastEvent(event) {
+        //   ApplicationListener.onApplicationEvent(event) {
+        //     ConfigFileApplicationListener.onApplicationEnvironmentPreparedEvent(event) {
+        //       Loader.load() {
+        //         PropertySourceLoader.load(name, resource)
+        //       }
+        //     }
+        //   }
+        // }
+      }
+    }
+    createApplicationContext() {
+      // registerAnnotationConfigProcessors(registry) {
+      //   registerPostProcessor(ConfigurationClassPostProcessor.class)
+      //   registerPostProcessor(AutowiredAnnotationBeanPostProcessor.class)
+      //   registerPostProcessor(RequiredAnnotationBeanPostProcessor.class)
+      //   registerPostProcessor(CommonAnnotationBeanPostProcessor.class)
+      // }
+    }
+    prepareContext(ctx) {
+      postProcessApplicationContext(ctx)
+      applyInitializers(ctx)
+      SpringApplicationRunListener.contextPrepared(ctx)
+      load(ctx, sources) {
+        // BeanDefinitionLoader.load() {
+        //   AnnotatedBeanDefinitionReader.registerBean(annotatedClass) {
+        //     ConditionEvaluator.shouldSkip(metadata)
+        //     processCommonDefinitionAnnotations(abd)
+        //     BeanDefinitionCustomizer.customize(abd)
+        //     BeanDefinitionRegistry.registerBeanDefinition(bd) {
+        //       DefaultListableBeanFactory.registerBeanDefinition(bn, bd)
+        //     }
+        //   }
+        // }
+      }
+      SpringApplicationRunListener.contextLoaded(ctx)
+    }
+    refreshContext(ctx) {
+      AbstractApplicationContext.refresh() {
+        prepareRefresh()
+        prepareBeanFactory(beanFactory)
+        postProcessBeanFactory(beanFactory)
+        invokeBeanFactoryPostProcessors(beanFactory) {
+          ConfigurationClassPostProcessor.postProcessBeanDefinitionRegistry(registry) {
+            checkConfigurationClassCandidate(beanDef)
+            ConfigurationClassParser.parse(candidates) {
+              ConditionEvaluator.shouldSkip(metadata, phase)
+              processMemberClasses(configClass, sourceClass) {
+                may_processConfigurationClass(candidate)
+              }
+              processPropertySource(sourceClass)
+              processComponentScan(sourceClass) {
+                ComponentScanAnnotationParser.parse(componentScan) {
+                  findCandidateComponents(basePackage) {
+                    new SimpleMetadataReader(resource)
+                    isCandidateComponent(metadataReader)
+                  }
+                  _postProcessBeanDefinition(candidate, beanName)
+                  _processCommonDefinitionAnnotations(abd)
+                  checkCandidate(beanName, candidate)
+                  _registerBeanDefinition(definitionHolder, registry)
+                }
+                _parse(bdCand)
+              }
+              processImports(configClass, sourceClass) {
+                _processImportSelector(candidate) {
+                  processImports(importClassNames)
+                }
+                _processImportBeanDefinitionRegistrar(candidate) {
+                  addImportBeanDefinitionRegistrar(configClass, registrar)
+                }
+                _processConfigurationClass(candidate)
+              }
+              _processImportResource(sourceClass)
+              _processBeanMethods(sourceClass)
+              _processInterfaces(configClass, sourceClass)
+              _processSuperClass(sourceClass)
+              
+              processDeferredImportSelectors() {
+                AutoConfigurationImportSelector.selectImports(annotationMetadata) {
+                  SpringFactoriesLoader.loadFactoryNames(EnableAutoConfiguration.class, classLoader)
+                }
+                processImports(configClass, sourceClass, importClassName)
+              }
+            }
+            ConfigurationClassParser.validate()
+            ConfigurationClassBeanDefinitionReader.loadBeanDefinitions(configClasses) {
+              TrackedConditionEvaluator.shouldSkip(configClass)
+              _registerBeanDefinitionForImportedConfigurationClass(configClass)
+              loadBeanDefinitionsForBeanMethod(configClass.getBeanMethods())
+              loadBeanDefinitionsFromImportedResources(configClass.getImportedResources())
+              loadBeanDefinitionsFromRegistrars(configClass.getImportBeanDefinitionRegistrars())
+            }
+            _processUntilNoNewCandidates()
+            _registerSingleton(importRegistry)
+          }
+          invokeBeanFactoryPostProcessors(registryProcessors, beanFactory) {
+            ConfigurationClassPostProcessor.postProcessBeanFactory(beanFactory) {
+              enhanceConfigurationClasses(beanFactory) {
+                new ConfigurationClassEnhancer()
+                _enhance(enhancer, configClass, beanClassLoader)
+              }
+              new ImportAwareBeanPostProcessor()
+              _addBeanPostProcessor(beanFactory, importAwareBeanPostProcessor)
+            }
+          }
+
+          // todo
+        }
+        registerBeanPostProcessors(beanFactory)
+        initMessageSource()
+        initApplicationEventMulticaster()
+        onRefresh()
+        registerListeners()
+        finishBeanFactoryInitialization(beanFactory)
+        finishRefresh()
+      }
+    }
+    SpringApplicationRunListener.started()
+    AppliationRunner.run(args)
+    SpringApplicationRunListener.running()
+  }
+}
